27d77fce4a41adef23b4d1dd01d45d8eab56705a
cailong <cailong@anne.com.cn>
2018-02-09 15:16:03 +0800

    1：process.env.NODE_ENV多一个空格造成环境判断失败的问题，2：生产环境js通过UglifyJsPlugin压缩，并后缀min，3：生产环境css压缩，并后缀min，4：添加scss和less的支持，除了加载less-loader和sass-loader依赖，还需要手动添加node-sass和less依赖

--- app/script/Greeter.js
+++ app/script/Greeter.js
@@ -12,13 +12,24 @@ import React, {Component} from 'react';
 import greetingConf from '../conf/greeting';
 
 import GreeterStyle from '../style/Greeter.css';
+import sassTest from '../style/sass-test.scss';
+import lessTest from '../style/less-test.less';
 
 class Greeter extends Component{
     render() {
         return (
             <div className={GreeterStyle.greeting}>
-                <h1>red color</h1>
+                <h1>hi colors</h1>
                 {greetingConf.greetText}
+                <p>
+                    <div className={sassTest.test}>sass test<i className={sassTest.aaa}>嵌套显示</i></div>
+                    <i className={sassTest.aaa}>元素和父级平行，但是sass调用的是嵌套的类，调用失败</i>
+                </p>
+                <p>
+                    <i className={lessTest["less-test2"]}>less test</i>
+                </p>
+
+
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-let sourceMapType = '';
-switch (process.env.NODE_ENV){
+let sourceMapType = '', uglifyCss = false;
+switch (process.env.NODE_ENV.trim()){
     case 'development':
         sourceMapType = 'eval-source-map';
         break;
@@ -19,66 +19,75 @@ switch (process.env.NODE_ENV){
         break
 }
 
+let plugins = [
+    new webpack.BannerPlugin('版权所有: cailong@gmail.com'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './app/index.tmpl.html')
+    }),
+    new CleanWebpackPlugin('dist/*.*', {
+        root: __dirname,
+        verbose: true,
+        dry: false,
+    }),
+];
+
+//非开发环境压缩
+if(process.env.NODE_ENV.trim() !== 'development'){
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+    }));
+    uglifyCss = true;
+}
+
+plugins.push(new ExtractTextPlugin('./[name].[chunkhash:8]'+(uglifyCss ? '.min' : '')+'.css'));
+
+var rules = [
+    {
+        test: /(\.js|\.jsx)$/,
+        use: {
+            loader: 'babel-loader',
+        },
+        exclude: /node_modules/
+    },
+    {
+        test: /(\.css|\.scss|\.less$)/,
+        include: /app/,
+        exclude: /(node_modules|bower_components)/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
+                loader: 'css-loader',
+                options: {
+                    minimize: uglifyCss, //压缩
+                    modules: true, // 指定启用css modules
+                    localIdentName: '[name]-[hash:5]' // 指定css的类名格式
+                }
+            },'sass-loader', 'less-loader']
+        }),
+    }
+];
+
 module.exports = {
     // entry: path.resolve(__dirname, 'app/index.js'),
     entry: {
-        main: path.resolve(__dirname, 'app/index.js'),
+        _index: path.resolve(__dirname, 'app/index.js'),
         underscore: ['underscore']
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].[hash:8].js'
+        filename: '[name].[hash:8]'+(process.env.NODE_ENV.trim() === 'development' ? '' : '.min')+'.js'
     },
     devtool: sourceMapType,
     devServer: {
-        contentBase: path.resolve(__dirname, './public'), //本地服务器所加载的页面所在的目录
+        contentBase: path.resolve(__dirname, './dist'), //本地服务器所加载的页面所在的目录
         historyApiFallback: true, //不跳转
         inline: true, //实时刷新
         hot: true,
         port: 3000,
     },
-    module: {
-        rules: [
-            {
-                test: /(\.js|\.jsx)$/,
-                use: {
-                    loader: 'babel-loader',
-                },
-                exclude: /node_modules/
-            },
-            {
-                test: /(\.css|\.scss|\.less$)/,
-                exclude: /(node_modules|bower_components)/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader']
-                }),
-                // use: [{
-                //     loader: 'style-loader'
-                // },{
-                //     loader: 'css-loader',
-                //     options: {
-                //         modules: true, // 指定启用css modules
-                //         localIdentName: '[name]-[hash:base64:5]' // 指定css的类名格式
-                //     }
-                // }]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.BannerPlugin('版权所有: cailong@gmail.com'),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-
-        new ExtractTextPlugin('./[name].[chunkhash:8].css'),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './app/index.tmpl.html')
-        }),
-        new CleanWebpackPlugin('dist/*.*', {
-            root: __dirname,
-            verbose: true,
-            dry: false,
-        }),
-    ]
+    module: {rules: rules},
+    plugins: plugins
 };
